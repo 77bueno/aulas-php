8c903fb4b47a2d620ea0b7073a825333ed95ca04
victor bueno <victorhugomotabueno@gmail.com>
2023-08-03 08:47:09 -0300

    Função: sub-rotina/procedimento

---
+++ 06-funcoes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções no PHP</title>
+</head>
+<body>
+<h1>Criando e usando funções</h1>
+<hr>
+
+<h2>Funções como procedimento ou sub-rotina</h2>
+<?php 
+function dadosAutor(){
+    echo "<div>";
+    echo "<p>Victor Hugo M. Bueno<p>";
+    echo "<p><a href=''>victorhugomotabueno@gmail.com</a></p>";
+    echo "</div>";
+}
+?>
+
+<section>
+    <h3>Chamada da sub-rotina/procedimento</h3>
+    <?=dadosAutor()?>
+    <article>
+        <h4>Outra chamada</h4>
+        <?=dadosAutor()?>
+    </article>
+</section>
+</body>
+</html>
