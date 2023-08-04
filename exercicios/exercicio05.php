0ecdc8500f62309fa40ef4aca20162ce48e620ee
victor bueno <victorhugomotabueno@gmail.com>
2023-08-04 09:18:27 -0300

    Exercício 5

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -21,27 +21,61 @@
 <?php
 
 $alunos = [
-    
-
+    [
+        "nome" => "Victor",
+        "nota1" => 8,
+        "nota2" => 9,
+    ],
+    [
+        "nome" => "Phelipe",
+        "nota1" => 5, 
+        "nota2" => 4,
+    ],
+    [
+        "nome" => "Jhon",
+        "nota1" => 6,
+        "nota2" => 6,
+    ],
+    [
+        "nome" => "Eliandro",
+        "nota1" => 9, 
+        "nota2" => 2,
+    ],
+    [
+        "nome" => "Eliel",
+        "nota1" => 7,
+        "nota2" => 5
+    ]
 ];
 
 
-function aluno(float $nota1, float $nota2):float{
+function media(float $nota1, float $nota2):float{
     $media = ($nota1 + $nota2) / 2;
 
     return $media;
 }
 
-function resultado(int $media):string{
+
+function resultado(float $media):string{
     if ($media >= 7) {
         return "aprovado";
     } else { 
         return "reprovado";
     }
 }
+
+foreach($alunos as $aluno){
+    ?>
+        <p>Nome: <?=$aluno["nome"]?> </p>
+        <p>Nota 1: <?=$aluno["nota1"]?> </p>
+        <p>Nota 2: <?=$aluno["nota2"]?> </p>
+        <p>E sua média foi: <?=media($aluno["nota1"], $aluno["nota2"])?></p>
+        <p>E você foi: <?=resultado(  media($aluno["nota1"], $aluno["nota2"])  )?></p>
+        <hr>
+    <?php
+}
 ?>
 
-<p>Sua média é: <?=aluno(6,8)?> e você está: <span class="<?=resultado(aluno(6,8))?>"><?=resultado(aluno(6,8))?></span> </p>
 
 </body>
 </html>
