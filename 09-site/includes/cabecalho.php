9333faeac0e354662752cd9d40310c8d9002e0f7
victor bueno <victorhugomotabueno@gmail.com>
2023-08-08 08:44:32 -0300

    tualização Title

--- 09-site/includes/cabecalho.php
+++ 09-site/includes/cabecalho.php
@@ -25,7 +25,7 @@ switch ($arquivos) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$titulo?></title>
+    <title><?=$titulo?> - Site PHP</title>
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
